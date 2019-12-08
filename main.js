29d1e93cd3230d93839bd1b5a3ce1a76d8bc1692
nico12450 <nicolas.gamel12450@gmail.com>
2019-12-08 12:11:56 +0100

    ajout d'une fonction somme

--- main.js
+++ main.js
@@ -4,8 +4,12 @@ const run = async () => {
   const buffer = readFileSync("./main.wasm");
   const module = await WebAssembly.compile(buffer);
   const instance = await WebAssembly.instantiate(module);
-  double = instance.exports.double
+  affiche1 = instance.exports.affiche1;
+  double = instance.exports.double;
+  somme = instance.exports.somme;
+  console.log(affiche1())
   console.log(double(10));
+  console.log(somme(10,20));
 };
 
 run();
--- main.wat
+++ main.wat
@@ -1,11 +1,18 @@
 (module
-  (func (param i32) (result i32)
-    i32.const 42
+  (func (result i32)
+    i32.const 1
   )
   (func (param i32) (result i32)
     get_local 0
     get_local 0
     i32.add
   )
+  (func (param i32) (param i32) (result i32)
+    get_local 0
+    get_local 1
+    i32.add
+  )
+  (export "affiche1" (func 0))
   (export "double" (func 1))
+  (export "somme" (func 2))
 )
